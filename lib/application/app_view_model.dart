4abf6b8df61a6a6ae8015d584305848300c2ca35
Abdullah Javaid <abdullah.javaid0331@gmail.com>
2024-06-11 23:59:21 +0500

    remove the appView Model and update the dio client

--- lib/application/app_view_model.dart
+++
@@ -1,3 +0,0 @@
-// import 'package:mukabbir/ui/base/base_view_model.dart';
-//
-// class AppViewModel extends BaseViewModel {}
---
+++ lib/constant/theme/text_theme.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kanban_board/constant/app_color/app_colors.dart';
+
+class AppTextTheme {
+  AppTextTheme._();
+
+  static TextTheme lightTheme(BuildContext context) {
+    return Theme.of(context)
+        .textTheme
+        .copyWith(
+          displayLarge: _buildTextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 54.sp,
+            color: AppColors.textColor,
+          ),
+          displayMedium: _buildTextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 32.sp,
+            color: AppColors.textColor,
+          ),
+          displaySmall: _buildTextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 28.sp,
+            color: AppColors.textColor,
+          ),
+          headlineLarge: _buildTextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 32.sp,
+            color: AppColors.textColor,
+          ),
+          headlineMedium: _buildTextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 20.sp,
+            color: AppColors.textPlaceHolderColor,
+          ),
+          headlineSmall: _buildTextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 18.sp,
+            letterSpacing: 0.5,
+            color: AppColors.textPlaceHolderColor,
+          ),
+          titleLarge: _buildTextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 20.sp,
+            color: AppColors.textPlaceHolderColor,
+          ),
+          titleMedium: _buildTextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 22.sp,
+            color: AppColors.textColor,
+          ),
+          titleSmall: _buildTextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20.sp,
+            color: AppColors.textColor,
+          ),
+          bodyLarge: _buildTextStyle(
+            fontSize: 16.sp,
+            color: AppColors.textColor,
+            fontWeight: FontWeight.w500,
+          ),
+          bodyMedium: _buildTextStyle(
+            fontSize: 14.sp,
+            fontWeight: FontWeight.w500,
+            color: AppColors.textColor,
+          ),
+          bodySmall: _buildTextStyle(
+            fontSize: 12.sp,
+            color: AppColors.textColor,
+          ),
+        )
+        .apply(fontSizeFactor: 1.sp);
+  }
+
+  static darkTheme(BuildContext context) {
+    return Theme.of(context).textTheme;
+  }
+
+  static TextStyle _buildTextStyle({
+    FontWeight fontWeight = FontWeight.normal,
+    double fontSize = 12.0,
+    double letterSpacing = 0,
+    Color color = Colors.red,
+  }) {
+    return TextStyle(
+      fontWeight: fontWeight,
+      fontSize: fontSize,
+      color: color,
+      letterSpacing: letterSpacing,
+    );
+  }
+}
