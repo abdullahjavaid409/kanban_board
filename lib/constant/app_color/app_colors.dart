d132d48a5117f77b76e4645cbe9079991c7a0298
Abdullah Javaid <abdullah.javaid0331@gmail.com>
2024-06-23 03:02:02 +0500

    update kanboard color

--- lib/constant/app_color/app_colors.dart
+++ lib/constant/app_color/app_colors.dart
@@ -6,7 +6,10 @@ class AppColors {
   const AppColors._();
 
   static const Color primaryColor = Color(0xFF6552FE);
-  static const Color textColor = Colors.black;
+  static const Color textColor = Color(0xFF000000);
   static const Color backgroundColor = Color(0xFFF6F7FB);
   static const Color lightBlack = Color(0x80000000);
+  static const greenAccent = Color(0xFF00C853);
+  static const redAccent = Color(0xFFD50000);
+  static const shadowColor = Colors.black12;
 }
