4306745f7625e9fd44cc0781dfa97fec2d8fc49b
Abdullah Javaid <abdullah.javaid0331@gmail.com>
2024-06-22 20:44:53 +0500

    update the color of theme class

--- lib/constant/app_color/app_colors.dart
+++ lib/constant/app_color/app_colors.dart
@@ -7,5 +7,6 @@ class AppColors {
 
   static const Color primaryColor = Color(0xFF6552FE);
   static const Color textColor = Colors.black;
-  static const Color backgroundColor = Colors.white;
+  static const Color backgroundColor = Color(0xFFF6F7FB);
+  static const Color lightBlack = Color(0x80000000);
 }
