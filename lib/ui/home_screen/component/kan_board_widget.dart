1bdd31e9143eabada927f2e2504cd7820188dc76
Abdullah Javaid <abdullah.javaid0331@gmail.com>
2024-06-22 20:51:28 +0500

    update kanboard card widget

--- lib/ui/home_screen/component/kan_board_widget.dart
+++ lib/ui/home_screen/component/kan_board_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kanban_board/constant/app_color/app_colors.dart';
 import 'package:kanban_board/constant/constants.dart';
 import 'package:kanban_board/data/models/kan_board_card_model.dart';
 import 'package:kanban_board/ui/home_screen/component/kanboard_dilaog.dart';
@@ -28,64 +29,68 @@ class KanBoardCardWidget extends ConsumerWidget {
     final comments = ref.watch(commentsProvider(cardModel));
     final timer = ref.watch(timerProvider(cardModel));
 
-    return InkWell(
-      onTap: () => onPressed(context),
-      child: Card(
-        margin: EdgeInsets.symmetric(horizontal: 1.w, vertical: 1.h),
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+    return KanBoardContainer(
+      onPressed: onPressed,
+      margin: EdgeInsets.only(top: 5.h, bottom: 5.h),
+      padding: const EdgeInsets.all(10.0),
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(12),
+      boxShadow: [
+        BoxShadow(
+          color: AppColors.lightBlack.withOpacity(0.1),
+          spreadRadius: 0.7,
+          offset: const Offset(0, 4),
+        ),
+      ],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
             children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: Text(
-                      cardModel.title,
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyLarge
-                          ?.copyWith(fontWeight: FontWeight.bold),
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  IconButton(
-                    visualDensity: const VisualDensity(horizontal: -4),
-                    onPressed: () {
-                      showEditCardDialog(context, cardModel, allCards, onEdit);
-                    },
-                    icon: Icon(Icons.edit),
-                  ),
-                ],
+              Expanded(
+                child: Text(
+                  cardModel.title,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyLarge
+                      ?.copyWith(fontWeight: FontWeight.w400),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
               ),
-              const VerticalSpacing(of: 5),
-              Text(
-                description,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: Theme.of(context).textTheme.bodySmall,
+              IconButton(
+                visualDensity: const VisualDensity(horizontal: -4),
+                onPressed: () {
+                  showEditCardDialog(context, cardModel, allCards, onEdit);
+                },
+                icon: const Icon(Icons.edit),
               ),
-              const VerticalSpacing(of: 10),
-              Row(
-                children: [
-                  if (timer.inSeconds > 0)
-                    IconTitleWidget(
-                      title: formatDuration(timer),
-                    ),
-                  const Spacer(),
-                  if (comments.isNotEmpty)
-                    IconTitleWidget(
-                      icon: Icons.chat_bubble_outline,
-                      title: '${comments.length}',
-                    ),
-                ],
-              ),
-              const VerticalSpacing(of: 10),
             ],
           ),
-        ),
+          const VerticalSpacing(of: 5),
+          Text(
+            description,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.bodySmall,
+          ),
+          const VerticalSpacing(of: 10),
+          Row(
+            children: [
+              if (timer.inSeconds > 0)
+                IconTitleWidget(
+                  title: formatDuration(timer),
+                ),
+              const Spacer(),
+              if (comments.isNotEmpty)
+                IconTitleWidget(
+                  icon: Icons.chat_bubble_outline,
+                  title: '${comments.length}',
+                ),
+            ],
+          ),
+          const VerticalSpacing(of: 10),
+        ],
       ),
     );
   }
