76c98e536824f5c969caa1ca5b874929282cb5a0
Abdullah Javaid <abdullah.javaid0331@gmail.com>
2024-06-23 03:03:14 +0500

    update detail screen clear chat after user enter and update the chat ui and button

--- lib/ui/home_screen/detail_screen/detail_screen.dart
+++ lib/ui/home_screen/detail_screen/detail_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kanban_board/constant/app_color/app_colors.dart';
 import 'package:kanban_board/data/models/kan_board_card_model.dart';
 import 'package:kanban_board/ui/home_screen/provider/detail_provider.dart';
 import 'package:kanban_board/widgets/common_widget.dart';
@@ -18,17 +20,20 @@ class KanBoardDetailScreen extends ConsumerStatefulWidget {
 
 class _KanBoardDetailScreenState extends ConsumerState<KanBoardDetailScreen> {
   late TextEditingController descriptionController;
+  late TextEditingController commentController;
 
   @override
   void initState() {
     super.initState();
     descriptionController =
         TextEditingController(text: widget.card.description);
+    commentController = TextEditingController();
   }
 
   @override
   void dispose() {
     descriptionController.dispose();
+    commentController.dispose();
     super.dispose();
   }
 
@@ -41,94 +46,122 @@ class _KanBoardDetailScreenState extends ConsumerState<KanBoardDetailScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.card.title),
-        backgroundColor: Colors.white,
+        title: Text(widget.card.title,
+            style: Theme.of(context).textTheme.bodyLarge),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            KanBoardTextField(
-              controller: descriptionController,
-              hintText: 'Description',
-              onChanged: (val) {
-                if (val == null) return;
-                ref.read(descriptionProvider(widget.card).notifier).state = val;
-                widget.card.description = val;
-              },
-            ),
-            const SizedBox(height: 10),
-            Text("Tracked Time: ${formatDuration(timer)}"),
-            const SizedBox(height: 10),
-            Row(
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                KanBoardTextField(
+                  controller: descriptionController,
+                  hintText: 'Description',
+                  maxLines: 10,
+                  onChanged: (val) {
+                    if (val == null) return;
+                    ref.read(descriptionProvider(widget.card).notifier).state =
+                        val;
+                    widget.card.description = val;
+                  },
+                ),
+                const VerticalSpacing(of: 10),
+                Text("Tracked Time: ${formatDuration(timer)}",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyLarge
+                        ?.copyWith(fontWeight: FontWeight.w500)),
+                const VerticalSpacing(of: 10),
+                Row(
+                  children: [
+                    Expanded(
+                        child: KanBoardElevatedButton(
+                            textStyle: Theme.of(context)
+                                .textTheme
+                                .bodyLarge
+                                ?.copyWith(color: Colors.white),
+                            color: AppColors.greenAccent,
+                            onTap: (_) => timerNotifier.start(),
+                            title: 'Start')),
+                    const HorizontalSpacing(of: 10),
+                    Expanded(
+                        child: KanBoardElevatedButton(
+                            textStyle: Theme.of(context)
+                                .textTheme
+                                .bodyLarge
+                                ?.copyWith(color: Colors.white),
+                            color: AppColors.redAccent,
+                            onTap: (_) => timerNotifier.stop(),
+                            title: 'Stop')),
+                    const SizedBox(width: 10),
+                    Expanded(
+                        child: KanBoardElevatedButton(
+                            color: AppColors.backgroundColor.withOpacity(0.9),
+                            onTap: (_) => timerNotifier.reset(),
+                            title: 'Reset')),
+                  ],
+                ),
+                const VerticalSpacing(),
+                Text("Comments",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyLarge
+                        ?.copyWith(fontWeight: FontWeight.w500)),
+                const VerticalSpacing(of: 10),
                 Expanded(
-                  child: ElevatedButton(
-                    onPressed: () => timerNotifier.start(),
-                    child: const Text("Start"),
+                  child: ListView.builder(
+                    itemCount: comments.length,
+                    padding: EdgeInsets.zero,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: ListTile(
+                          contentPadding: EdgeInsets.only(left: 20.w),
+                          title: Text(comments[index],
+                              style: Theme.of(context).textTheme.bodyMedium),
+                          trailing: PopupMenuButton<String>(
+                            onSelected: (value) {
+                              if (value == 'Edit') {
+                                _showEditCommentDialog(context,
+                                    commentsNotifier, index, comments[index]);
+                              } else if (value == 'Delete') {
+                                commentsNotifier.deleteComment(index);
+                                displayToast("Successfully Delete comment");
+                              }
+                            },
+                            itemBuilder: (context) => [
+                              const PopupMenuItem(
+                                value: 'Edit',
+                                child: Text('Edit'),
+                              ),
+                              const PopupMenuItem(
+                                value: 'Delete',
+                                child: Text('Delete'),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
                   ),
                 ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () => timerNotifier.stop(),
-                    child: const Text("Stop"),
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () => timerNotifier.reset(),
-                    child: const Text("Reset"),
-                  ),
+                KanBoardTextField(
+                  controller: commentController,
+                  hintText: 'Add a comment',
+                  onFieldSubmitted: (val) {
+                    if (val == null || val.isEmpty) return;
+                    commentsNotifier.addComment(val);
+                    commentController.clear();
+                    FocusScope.of(context).unfocus();
+                  },
                 ),
               ],
             ),
-            const SizedBox(height: 20),
-            const Text("Comments"),
-            const SizedBox(height: 10),
-            Expanded(
-              child: ListView.builder(
-                itemCount: comments.length,
-                padding: EdgeInsets.zero,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(comments[index]),
-                    trailing: PopupMenuButton<String>(
-                      onSelected: (value) {
-                        if (value == 'Edit') {
-                          _showEditCommentDialog(context, commentsNotifier,
-                              index, comments[index]);
-                        } else if (value == 'Delete') {
-                          commentsNotifier.deleteComment(index);
-                        }
-                      },
-                      itemBuilder: (context) => [
-                        PopupMenuItem(
-                          value: 'Edit',
-                          child: Text('Edit'),
-                        ),
-                        PopupMenuItem(
-                          value: 'Delete',
-                          child: Text('Delete'),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ),
-            TextField(
-              onSubmitted: (val) {
-                commentsNotifier.addComment(val);
-              },
-              decoration: const InputDecoration(
-                labelText: "Add a comment",
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
